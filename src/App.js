a740dae7087b862f1982216d4bb97647536b4fe6
Joemol Joy <joemoljoy.a@gmail.com>
2026-02-08 13:51:40 -0500

    Cer element add and remove button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart, removeFromCart } from './redux/cartSlice'
+
 
 function App() {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+
+  const addItem = () => {
+    dispatch(addToCart({id:1,  name: 'Burger' }));
+  };
+
+  const removeItem = (id) => {
+    dispatch(removeFromCart(id));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div style={{padding: '20px'}}>
+     <h1>Redux Shopping Cart</h1>
+     <button onClick={addItem}>Add Burger</button>
+
+     <h2>Cart Items:</h2>
+     {cart.map((item, index) => (
+      <div key={item.id}>
+        <span>{item.name}</span>
+        <button onClick={() => removeItem(item.id)}>Remove</button>
+      </div>
+     ))}
+   </div>
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './cartSlice'
 
 export const store = configureStore({
   reducer: {
     // reducers will go here
+    cart: cartReducer
   }
 })
